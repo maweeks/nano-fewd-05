6fe4695d7d3a5ba4fcfb7a6775e399c9d832e696
Matt Weeks <m_weeks@hotmail.co.uk>
2015-04-29 15:02:05 +0100

    Center map.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
 // Models
 var mapData = {
 	locations: [ 
-		{placeId: "ChIJ_bpIw7XL3kcR09ogp8rgfCg"}, //Black Griffin
+
+	//Ballroom
+	//Black Griffin
+	//Shakespear
+		{placeId: "ChIJh8UibbXL3kcRSeRb7F8fDDo"}, //Ball Room
+		//{placeId: "ChIJ_bpIw7XL3kcR09ogp8rgfCg"}, //Black Griffin
 		{placeId: "ChIJ6Z00V8nL3kcRhNSJi_KNoIE"}, //Chemistry
 		{placeId: "ChIJhTdHADXK3kcRpnOnvhy-6lQ"}, //Chill
 		{placeId: "ChIJB40BgMrL3kcRr07WbPUriKk"}, //Cuban
+		{placeId: "ChIJ756NpcrL3kcRftgyPZzEosI"}, //Essence
+		{placeId: "ChIJD56WI0vK3kcRr9FHIPvV7Tc"}, //Jolly Sailor
 		{placeId: "ChIJeWgu2rXL3kcRfYSayANVg2c"}, //Lady Luck
 		{placeId: "ChIJZSe6hEvK3kcRJuT5Xkt0xzI"}, //Penny
+		{placeId: "ChIJrRZKbbXL3kcRFZEAiJtAVow"}, //Seven Stars
 		{placeId: "ChIJfdVmUMrL3kcRwCu3hudCZpo"}  //Three Tuns
 	], //https://developers.google.com/maps/documentation/javascript/examples/places-placeid-finder
 	options: {
-		center: { lat: 51.281405, lng: 1.075951},
+		center: { lat: 51.2784, lng: 1.0826},
 		zoom: 15
 	}
 };
@@ -32,12 +40,6 @@ var ModelView = function() {
 
 function initialize() {
 	var map = new google.maps.Map(document.getElementById('map-canvas'), mapData.options);
-
-	var request = {
-		placeId: 'ChIJ_bpIw7XL3kcR09ogp8rgfCg'
-	};
-
-	var i=0;
 	for (var i = 0; i < mapData.locations.length; i++) {
 		var request = mapData.locations[i];
 	 	var infowindow = new google.maps.InfoWindow();
