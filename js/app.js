a3d34bd96467375ab48d8c6959f4cffc4d66f4cc
Matt Weeks <m_weeks@hotmail.co.uk>
2015-05-03 15:44:49 +0100

    Add empty details.

--- js/app.js
+++ js/app.js
@@ -36,20 +36,25 @@ var ViewModel = function() {
 
 // Views
 var DetailView = {
+	createMapData: function() {
+		var selected = mapData.selected;
+		console.log(selected);
+	},
 	renderNotShowing: function () {
-		var detailsElement = document.getElementById('detaitsContent');
+		var detailsElement = document.getElementById('detailsContent');
 		detailsElement.className = detailsElement.className.replace(' notNow', '');
 		detailsElement.className += ' notNow';
 	},
 	renderShowing: function() {
 		// console.log(selected);
-		var detailsElement = document.getElementById('detaitsContent');
+		var detailsElement = document.getElementById('detailsContent');
 		detailsElement.className = detailsElement.className.replace(' notNow', '');
 		if (mapData.selected != "") {
-			console.log("Showing " + selected.name);
+			DetailView.createMapData();
 		}
 		else {
 			console.log("Showing empty");
+			document.getElementById('detailsInfo').innerHTML = '<h3>Nothing currently selected.</h3><em> Select a pin on the map to view more details. </em>';
 		}
 	}
 };
